465545974a370d00e8dc6d3a883a6ca6af9e0ba6
voidReq <100312344+voidReq@users.noreply.github.com>
2024-07-11 23:45:31 -0500

    Update dump.py

---
+++ dump.py
@@ -0,0 +1,39 @@
+import os
+import platform
+from getmac import get_mac_address as gma
+import socket
+import urllib.request
+
+
+
+format = "---------------------------------------------------"
+directory = os.getcwd()
+
+
+def dump():
+  hostname = socket.gethostname()
+  privateIP = socket.gethostbyname(hostname)
+  publicIP = external_ip = urllib.request.urlopen(
+      'https://ident.me').read().decode('utf8')
+  mac = gma()
+  user = os.getlogin()
+  system = platform.system()
+  release = platform.release()
+  version = platform.version()
+
+  allAtOnce = f"""
+Hostname: {hostname}
+{format}
+Public IP Address: {publicIP}
+{format}
+Private IP Address: {privateIP}
+{format}
+Mac address: {mac}
+{format}
+User: {user}
+{format}
+OS info: \n---{system}\n---{release}\n---{version}
+{format}
+CWD: {os.getcwd()}
+{format}
+"""
